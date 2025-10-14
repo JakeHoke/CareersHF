0d18505bbd51a736ae59ee515984e5b5408433df
JakeHoke <50216434+JakeHoke@users.noreply.github.com>
2025-10-13 21:31:40 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showModal(jobTitle) {
-    document.getElementById('modal').style.display = 'flex';
-    document.getElementById('modal-job-title').textContent = `Apply for ${jobTitle}`;
-}
-
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-}
